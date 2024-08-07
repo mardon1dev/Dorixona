87eca0976d5040563bc0d33cba60823f74d53d2f
mardon1dev <dusbekovmardonbek@gmail.com>
2024-08-08 04:51:14 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,34 @@ burger.addEventListener("click", ()=>{
     menu.classList.toggle("open");
     burger.classList.toggle("change")
 })
+
+let video = document.querySelector(".video__video");
+let play = document.querySelector(".video__play");
+let pause = document.querySelector(".video__pause");
+
+play.addEventListener("click", ()=>{
+    video.play();
+    play.style.display = "none";
+})
+
+video.addEventListener("click", ()=>{
+    video.pause();
+    play.style.display = "grid"
+})
+
+const backtotop = document.querySelector(".top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    backtotop.style.display = "flex";
+  } else {
+    backtotop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,60 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".myStore", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
-  });
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    }
+  }
+  
+});
+var swiper = new Swiper(".myReviews", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
